a0617f02236f3638286663ab45b58300e6e73202
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-07-20 14:54:16 +0500

    added Unit test

--- app/src/test/java/com/shahriyor/android_graphql/ExampleUnitTest.kt
+++ app/src/test/java/com/shahriyor/android_graphql/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.shahriyor.android_graphql
 
+import com.shahriyor.android_graphql.network.GraphQL
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +15,24 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun checkUsersListSize() = runTest {
+        val response = GraphQL.get().query(UserListQuery(12)).execute()
+        assertEquals(10, response.data!!.users.size)
+    }
+
+    @Test
+    fun checkInsertUser() = runTest {
+        val response = GraphQL.get().mutation(InsertUserMutation("Salom", "rocket", "twitter")).execute()
+        assertEquals(1, response.data?.insert_users?.affected_rows)
+    }
+
+    @Test
+    fun checkDeleteUser() = runTest {
+        val response = GraphQL.get().mutation(DeleteUserMutation("f622720e-63b7-4ff2-9d34-20aa294fc0a2")).execute()
+        assertEquals(1, response.data?.delete_users?.affected_rows)
+
     }
 }
